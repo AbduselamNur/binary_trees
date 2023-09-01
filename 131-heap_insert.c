a4e4276baef2e8f0a3ec5752e14854eaf561874f
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-09-01 16:25:48 +0300

    heap insert

---
+++ 131-heap_insert.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *tr, *new, *flip;
+	int size, leaves, sub, bit, level, tmp;
+
+	if (!root)
+		return (NULL);
+	if (!(*root))
+		return (*root = binary_tree_node(NULL, value));
+	tr = *root;
+	size = binary_tree_size(tree);
+	leaves = size;
+	for (level = 0, sub = 1; leaves >= sub; sub *= 2, level++)
+		leaves -= sub;
+
+	for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
+		tr = leaves & bit ? tr->right : tr->left;
+
+	new = binary_tree_node(tr, value);
+	leaves & 1 ? (tr->right = new) : (tr->left = new);
+
+	flip = new;
+	for (; flip->parent && (flip->n > flip->parent->n); flip = flip->parent)
+	{
+		tmp = flip->n;
+		flip->n = flip->parent->n;
+		flip->parent->n = tmp;
+		new = new->parent;
+	}
+
+	return (new);
+}
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
